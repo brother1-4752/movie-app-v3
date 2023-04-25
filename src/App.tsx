089121041aa2bbf5938d76ec260134cc871cc091
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-04-25 23:57:33 +0900

    feat : 리액트-fontawesome 적용
    - individualy만 적용
    - 동적은 아직 못함

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import { darkTheme, lightTheme } from "./theme";
 import GlobalStyle from "./GlobalStyle";
 import Home from "./Routes/Home";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const isDark = useRecoilValue(isDarkAtom);
@@ -20,6 +23,12 @@ function App() {
         <AppBtn onClick={handleMode}>
           {isDark ? "라이트모드" : "다크모드"}
         </AppBtn>
+        <div>zzzzz</div>
+        <FontAwesomeIcon icon={faBars} />
+        {/* <FontAwesomeIcon
+          className="header__search--icon"
+          icon={solid("magnifying-glass")}
+        /> */}
       </ThemeProvider>
     </>
   );
